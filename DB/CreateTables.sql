46f4c04b47f05b4c536ec9ed4ab7379b99c7f19f
SquadElves <tima.bala1612@gmail.com>
2023-06-12 07:26:37 +0300

    Added tables auctions and bids

--- DB/CreateTables.sql
+++ DB/CreateTables.sql
@@ -2,6 +2,22 @@ create table users(
     ID serial primary key,
     Name varchar(20),
     Surname varchar(20),
-    UserName unic varchar(20)
+    UserName varchar(20) unique,
+    DateRegistration timestamp
 );
-
+create table auctions(
+    ID serial primary key,
+    IDCreator int references users(ID),
+    Title varchar(100),
+    TimeStart timestamp,
+    TimeEnd timestamp,
+    PriceBuyNow int,
+    Price int
+);
+create table bids(
+     IDAuction int not null references auctions(ID),
+     IDUser int not null references users(ID),
+     Amount int,
+     Date timestamp,
+     primary key (IDAuction, IDUser)
+);
