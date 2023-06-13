79b1b068dafc65b8f8dc9d1e997b72ccba3cd8ae
SquadElves <tima.bala1612@gmail.com>
2023-06-13 06:08:12 +0300

    added file with DB settings

---
+++ Bids&Auctions/app/database/ConnectDB.php
@@ -0,0 +1,12 @@
+<?php
+    $dbms = 'pgsql';
+    $host = 'localhost';
+    $dbname = 'auction';
+    $username = 'postgres';
+    $password = "11111111";
+    try{
+        $pdo = new PDO("$dbms:dbname=$dbname;host=$host", $username, $password);
+
+    } catch (PDOException $exception){
+        die("Error while connecting to DataBase");
+    }
